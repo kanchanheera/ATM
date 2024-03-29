0cef68d0f34618fb87427f9c30307439b25dec0b
Kanchan Heera <161306339+kanchanheera@users.noreply.github.com>
2024-03-29 17:33:46 +0500

    Created an ATM

---
+++ main.ts
@@ -0,0 +1,71 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+let balance = 10000; // Dollar
+let pinCode = 7890;
+
+let pinAnswer = await inquirer.prompt(
+    [ 
+        {
+            name: "pin",
+            message: "Enter your pin",
+            type: "number"
+        }
+    ]
+);
+
+if (pinAnswer.pin === pinCode ) {
+    console.log("Correct pin code!");
+
+let operationAns = await inquirer.prompt(
+        [
+            {
+                name: "operation",
+                message: "please select option",
+                type: "list",
+                choices: ["withdraw", "check balance", "fast cash"]
+            }
+        ]
+    );
+
+if (operationAns.operation === "withdraw") {
+let amountAns = await inquirer.prompt(
+            [
+                {
+                   name: "amount",
+                   message: "enter your amount",
+                   type: "number"
+                }
+            ]
+        );
+
+        if (amountAns.amount > balance) {
+            console.log("Insufficient Balance");
+        } else if( balance -= amountAns.amount) {
+        console.log(`Your remaining balance is: ${balance}`);
+
+     }
+
+    } else if(operationAns.operation === "check balance") {
+        console.log(`Your balance is: ${balance}`);
+
+       
+} else if(operationAns.operation === "fast cash") {
+    let fastCashAns = await inquirer.prompt(
+            [
+                {
+                   name: "cash",
+                   message: "Select the amount you'd like to cash fast: ",
+                   type: "list",
+                   choices: ["1000", "2000", "3000", "4000", "5000", "1000"]
+                }
+            ]
+        );     
+    }   
+} 
+
+
+else  {
+    console.log("Incorrect pin code");
+}
